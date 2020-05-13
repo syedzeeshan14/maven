e27833106abfa6df1157c863443ed337fdbe406a
Lenovo <Lenovo@192.168.1.207>
2020-05-13 12:07:45 +0530

    New Code Enter

---
+++ Jen/src/test/java/Jenkins/Jen/test.java
@@ -0,0 +1,12 @@
+package Jenkins.Jen;
+
+import org.testng.annotations.Test;
+import org.testng.annotations.Test;
+
+public class test {
+@Test
+public void tt()
+{
+	System.out.println("I am Test annotation");
+}
+}
